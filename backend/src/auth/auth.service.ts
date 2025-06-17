967de455482621b8f51d8c0a03ff61232c674b6e
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-17 13:20:06 +0000

    Created sign in service (without password encryption yet)

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
+import { UserDTO } from 'src/dtos/user.dto';
+import { SignInResult } from 'src/interfaces/sign-in-result.interface';
 import { User } from 'src/interfaces/user.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @Inject('USER_MODEL')
     private userModel: Model<User>,
     private jwtService: JwtService,
   ) {}
 
-  signIn() {
-    
+  async signIn(dto: UserDTO): Promise<SignInResult> {
+    const userExists = await this.userModel.findOne({ $or: [{ username: dto.username }, { email: dto.email }] });
+    if (userExists) {
+      throw new Error(`User with username ${dto.username} or email ${dto.email} already exists`);
+    }
+
+    const user = new this.userModel(dto);
+    const { username, email } = await user.save();
+
+    return {
+      username,
+      email,
+      token: await this.jwtService.signAsync({ username, email })
+    }
   }
 }
---
+++ backend/src/interfaces/sign-in-result.interface.ts
@@ -0,0 +1,5 @@
+export interface SignInResult {
+  username: string;
+  email: string;
+  token: string;
+}

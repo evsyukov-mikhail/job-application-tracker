88cdd81817f10920c6a182923a6c1f570bd0b18a
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-17 12:38:38 +0000

    Injected necessary dependencies into Auth module

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  @Post('/signin')
+  async signIn() {
+    
+  }
+
+  @Post('/login')
+  async login() {
+    
+  }
+
+}

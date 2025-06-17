a2dabebe0b2baffe4623293559d7a9a6c538330d
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-17 12:32:10 +0000

    Created Auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JobApplicationsController } from './job-applications/job-applications.controller';
 import { JobApplicationsModule } from './job-applications/job-applications.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [JobApplicationsModule],
+  imports: [JobApplicationsModule, AuthModule],
 })
 export class AppModule {}

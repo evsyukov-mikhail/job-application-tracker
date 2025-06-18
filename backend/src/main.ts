bf2a996e651dcf026a2f7d1b7b95f31be84cdb02
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-18 16:20:52 +0000

    Added redis microservice

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: 'job-appl-tracker-redis',
+      port: 6379,
+    },
+  });
+  await app.listen();
 }
 bootstrap();

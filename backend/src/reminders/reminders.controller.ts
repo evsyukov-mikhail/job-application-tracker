579662d84c8ad54e7839d209bbdf590bcf0766ba
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-19 11:54:27 +0000

    Finished reminders controller

--- backend/src/reminders/reminders.controller.ts
+++ backend/src/reminders/reminders.controller.ts
@@ -2,9 +2,13 @@ import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common
 import { Response } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { ReminderDTO } from 'src/dtos/reminder.dto';
+import { RemindersService } from './reminders.service';
 
 @Controller('reminders')
 export class RemindersController {
+  constructor(
+    private remindersService: RemindersService,
+  ) {}
 
   @Get()
   @UseGuards(AuthGuard)
@@ -13,7 +17,8 @@ export class RemindersController {
     @Res() res: Response,
   ) {
     try {
-
+      const reminders = await this.remindersService.findAllReminders(req.userId);
+      return res.status(200).json(reminders);
     } catch (error) {
       return res.status(400).json({ message: (error as Error).message });
     }
@@ -27,7 +32,8 @@ export class RemindersController {
     @Body() dto: ReminderDTO,
   ) {
     try {
-
+      const createdReminder = await this.remindersService.createReminder(req.userId, dto);
+      return res.status(200).json(createdReminder);
     } catch (error) {
       return res.status(400).json({ message: (error as Error).message });
     }

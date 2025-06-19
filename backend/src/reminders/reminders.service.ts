0602d87ff8798ce2e3c1b533c870a1514eb71b44
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-19 10:43:57 +0000

    Finished reminders service

--- backend/src/reminders/reminders.service.ts
+++ backend/src/reminders/reminders.service.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
+import { ReminderDTO } from 'src/dtos/reminder.dto';
+import { Reminder } from 'src/interfaces/reminder.interface';
 
 @Injectable()
 export class RemindersService {
   constructor(
+    @Inject('REMINDER_MODEL')
+    private reminderModel: Model<Reminder>,
   ) {}
 
-  async findAllReminders(userId: string) {
-
+  async findAllReminders(userId: string): Promise<Reminder[]> {
+    return this.reminderModel.find({ userId });
   }
 
-  async createReminder(userId: string) {
-
+  async createReminder(userId: string, dto: ReminderDTO): Promise<Reminder> {
+    const reminder = new this.reminderModel({ ...dto, userId });
+    return reminder.save();
   }
 }

00da35e328679b3c94c6443c6f36a1c9e4bfe0af
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-19 06:41:42 +0000

    Finished CacheModule registration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,23 @@ import { JobApplicationsModule } from './job-applications/job-applications.modul
 import { AuthModule } from './auth/auth.module';
 import { CryptoService } from './crypto/crypto.service';
 import { CryptoModule } from './crypto/crypto.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Keyv, createKeyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
 
 @Module({
   imports: [
-    JobApplicationsModule, AuthModule, CryptoModule
+    JobApplicationsModule, AuthModule, CryptoModule,
+    CacheModule.register({
+      useFactory: async () => ({
+        stores: [
+          new Keyv({
+            store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+          }),
+          createKeyv(`${process.env.REDIS_CONNECTION_STRING}`),
+        ],
+      })
+    })
   ],
   providers: [CryptoService],
 })

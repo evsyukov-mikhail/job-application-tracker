1fb1e61415bd76fa13319539b32aa64a5f0194b4
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-06-25 11:01:16 +0000

    Changed home page metadata

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Home" },
+    { name: "Job Application Tracker Home", content: "Home" },
   ];
 }
 

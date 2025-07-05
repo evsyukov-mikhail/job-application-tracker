440126beca5b32b682772b9af41cc803686fee74
evsyukov-mikhail <evsukovmikhail08@gmail.com>
2025-07-05 14:57:53 +0000

    Finished auth controller tests

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -2,14 +2,25 @@ import * as dotenv from 'dotenv';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Test } from '@nestjs/testing';
-import { CryptoModule } from 'src/crypto/crypto.module';
+import { CryptoModule } from '../crypto/crypto.module';
 import { JwtModule, JwtService } from '@nestjs/jwt';
-import { DatabaseModule } from 'src/database/database.module';
+import { AuthResult } from 'src/interfaces/auth-result.interface';
+import { Request, Response } from 'express';
 dotenv.config();
 
 describe('AuthController', () => {
   let controller: AuthController;
   let service: AuthService;
+
+  const mockRequest = {
+    query: {},
+    userId: '',
+  } as unknown as Request & { userId: string };
+
+  const mockResponse = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(x => x),
+  } as unknown as Response;
 
   beforeAll(async () => {
     const mockUserModel = {
@@ -40,4 +51,38 @@ describe('AuthController', () => {
 
   it('AuthController defined', () => expect(controller).toBeDefined());
   it('AuthService defined', () => expect(service).toBeDefined());
+
+  it('Sign up', async () => {
+    const mockResult = {
+      username: 'John Doe',
+      email: 'johndoe@gmail.com',
+      token: 'aaa.bbb.ccc',
+    } as AuthResult;
+    jest.spyOn(service, 'signUp').mockResolvedValue(mockResult);
+
+    const mockDTO = {
+      username: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: 'johndoe12345',
+    };
+    const result = await controller.signUp(mockResponse, mockDTO);
+    expect(result).toBe(mockResult);
+  })
+
+  it('Log in', async () => {
+    const mockResult = {
+      username: 'John Doe',
+      email: 'johndoe@gmail.com',
+      token: 'aaa.bbb.ccc',
+    } as AuthResult;
+    jest.spyOn(service, 'logIn').mockResolvedValue(mockResult);
+
+    const mockDTO = {
+      username: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: 'johndoe12345',
+    };
+    const result = await controller.login(mockResponse, mockDTO);
+    expect(result).toBe(mockResult);
+  })
 })
